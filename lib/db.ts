eca7df65efd508931ea85938c66785cb2c702c9f
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-04-14 11:17:51 +0530

    DB connection

---
+++ lib/db.ts
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error("Check DB connection string");
+}
+
+// from types.d.ts
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function ConnectToDatabase() {
+  // if already connected
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: true,
+      maxPoolSize: 10,
+    };
+
+    cached.promise = mongoose
+      .connect(MONGODB_URI, opts)
+      .then(() => mongoose.connection);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+  }
+
+  return cached.conn;
+}
